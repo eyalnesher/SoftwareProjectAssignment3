8d282a15707eae7b94728b7fce00a9484182ee38
eyalnesher <eyalnesher@mail.tau.ac.il>
2019-12-31 10:41:46 +0200

    Add interface of the `solver

--- SoftwareProjectAssignment3/solver.h
+++ SoftwareProjectAssignment3/solver.h
@@ -2,6 +2,18 @@
 #ifndef SOLVER_H
 #define SOLVER_H
 
+#include "game.h"
 
+/**
+ * Generate a board with `fixed` initial fixed cells.
+ */
+void generate_board(SudokuBoard* board, int fixed);
+
+/**
+ * Solve the board from its current state.
+ * Change the board to the first solution found (if such solution exists).
+ * Return if the board is solvable.
+ */
+bool validate_board(SudokuBoard* board);
 
 #endif /* !SOLVER_H */
