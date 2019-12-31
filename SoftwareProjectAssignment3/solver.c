88a425c9a0034d682b78676bf515c083af8df364
eyalnesher <eyalnesher@mail.tau.ac.il>
2019-12-31 10:44:24 +0200

    Add interface of the `solver` module (with documentation).

---
+++ SoftwareProjectAssignment3/solver.c
@@ -0,0 +1,24 @@
+
+#include "solver.h"
+
+/**
+ * Solve the board using backtracking algorithm, with the function
+ * `legal_cell_values` to iterate through the values (the function
+ * passes to the caller an array of values in the range [1, `max_value`],
+ * ordered in the iteration order, and returns the number of legal values).
+ * Return if the board is solvable.
+ */
+ /* TODO */
+static bool solve_board(SudokuBoard* board, size_t(*legal_cell_values)(int* values, size_t max_value)) {
+	return True;
+}
+
+/* TODO */
+void generate_board(SudokuBoard* board, int fixed) {
+
+}
+
+/* TODO */
+bool validate_board(SudokuBoard* board) {
+	return True;
+}
