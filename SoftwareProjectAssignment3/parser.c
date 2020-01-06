d2a812a4b0fcacab2a1e83358814c50d700538ed
nir <hihialhi@gmail.com>
2020-01-06 23:17:34 +0200

    added a parser

--- SoftwareProjectAssignment3/parser.c
+++ SoftwareProjectAssignment3/parser.c
@@ -1,16 +1,18 @@
 
 #include "parser.h"
 
+#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * Split a string into an array of `max_args` strings, using `delimiter` as a delimiter,
  * and pass it to the caller through `parsed_string`.
  * Return the number of the generated strings.
  */
-static int parse_deliminator(char* str, char* delimiter, char** parsed_string, size_t max_args) {
+static int parse_delimeter(char* str, char* delimiter, char** parsed_string, size_t max_args) {
 	char* token;
-	int count = 0;
+	size_t count = 0;
 
 	token = strtok(str, delimiter);
 
@@ -22,9 +24,93 @@ static int parse_deliminator(char* str, char* delimiter, char** parsed_string, s
 	return count;
 }
 
-/* TODO */
-int parse_and_run(char* str_input, int str_len) {
-	char* parsed_string[MAX_ARG_CNT];
-	int arg_count = parse_deliminator(str_input, " ", parsed_string, MAX_ARG_CNT);
+/**
+ * Gets input from stdin, parses the input using parse_delimeter and calculates the function's name and arguments
+ */
+static int get_argumets(char** function_name, size_t* function_args, size_t* function_args_size) {
+	char line[MAX_CHAR_CNT];
+	char* parsed_func[MAX_ARG_CNT];
+	size_t arg_cnt;
+	size_t i;
+
+	if (fgets(line, MAX_CHAR_CNT, stdin) == NULL) {
+		return -1; /* EOF encountered */
+	}
+	line[strcspn(line, "\n")] = 0;
+
+	arg_cnt = parse_delimeter(line, " ", parsed_func, MAX_ARG_CNT + 1);
+	
+
+	if (arg_cnt < 0) {
+		return -1; /* line is empty? */
+	}
+
+	*function_args_size = arg_cnt - 1;
+	*function_name = parsed_func[0];
+	for (i = 1; i < arg_cnt; i++) {
+		function_args[i - 1] = strtol(parsed_func[i], (char**)NULL, 10);
+	}
+
 	return 0;
+}
+
+/**
+ * Gets a Sudoku board, receives input from stdin and runs the command it was given
+ */
+int get_and_run(SudokuBoard* board) {
+	char* func_name;
+	size_t func_args[MAX_ARG_CNT];
+	size_t arg_count;
+	
+	get_argumets(&func_name, func_args, &arg_count);
+
+	return run_command(board, func_name, func_args, arg_count);
+}
+
+/**
+ * Test the equality of the function name and function argument count to the tested_name and tested_args_count
+ */
+static int test_args(char* func_name, size_t args_count, char* tested_name, size_t tested_args_count) {
+	return func_name != NULL && tested_name != NULL && strcmp(func_name, tested_name) == 0 && args_count == tested_args_count;
+}
+
+
+/* TODO - RETURN VALUE */
+int run_command(SudokuBoard* board, char* func_name, size_t* func_args, size_t args_count) {
+	int hint;
+
+	/* syntax: set X Y Z; meaning: set column X row Y cell value to Z */
+	if (test_args(func_name, args_count, "set", 3)) {
+		set_cell_value(board, func_args[1] - 1, func_args[0] - 1, func_args[2]);
+		return 0;
+	}
+
+	/* syntax: hint X Y; meaning: get a hint on column X row Y cell */
+	if (test_args(func_name, args_count, "hint", 2)) {
+		get_cell_hint(board, func_args[1] - 1, func_args[0] - 1, &hint);
+		return 0;
+	}
+
+	/* syntax: validate; meaning: checks to see whether the current board state is solveable */
+	if (test_args(func_name, args_count, "restart", 0)) {
+		if (!validate_board(board)) {
+			printf("Validation failed: board is unsolvable\n");
+		}
+		return 0;
+	}
+
+	/* syntax: restart; meaning: restart the game, creating a new puzzle */
+	if (test_args(func_name, args_count, "validate", 0)) {
+		restart_game(board);
+		return 0;
+	}
+
+	/* syntax: exit; meaning: exit the game, freeing all memory resources */
+	if (test_args(func_name, args_count, "exit", 0)) {
+		exit_game();
+		return 0;
+	}
+
+	printf("Error: invalid command\n");
+	return -1;
 }
--- SoftwareProjectAssignment3/parser.h
+++ SoftwareProjectAssignment3/parser.h
@@ -2,12 +2,20 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "game.h"
+
 #define MAX_ARG_CNT (4)
+#define MAX_CHAR_CNT (1024)
 
 /**
- * Parse a commant from the user and runs it.
+ * Runs the given coomand
  * Return 0 on success, -1 otherwise.
  */
-int parse_and_run(char* str_input, int str_len);
+int run_command(SudokuBoard* board, char* func_name, size_t* func_args, size_t args_count);
+
+/**
+ * Gets a command and its arguments through stdin, and runs the command
+ */
+int get_and_run(SudokuBoard* board);
 
 #endif /* !PARSER_H */
