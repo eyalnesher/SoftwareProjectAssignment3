d387ff4195a309fa484a8c37805d17373fda11d9
eyalnesher <eyalnesher@mail.tau.ac.il>
2020-01-07 15:10:13 +0200

    Revert "Revert "Fix differences between the instructions and our program.""
    This reverts commit 32bd26cfe0a5f358125d92789f9a651c5e80c1a1.

--- SoftwareProjectAssignment3/parser.h
+++ SoftwareProjectAssignment3/parser.h
@@ -8,13 +8,13 @@
 #define MAX_CHAR_CNT (1024)
 
 /**
- * Runs the given coomand
+ * Run the given coomand
  * Return 0 on success, -1 otherwise.
  */
 int run_command(SudokuBoard* board, char* func_name, size_t* func_args, size_t args_count);
 
 /**
- * Gets a command and its arguments through stdin, and runs the command
+ * Get a command and its arguments through stdin, and runs the command
  */
 int get_and_run(SudokuBoard* board);
 
